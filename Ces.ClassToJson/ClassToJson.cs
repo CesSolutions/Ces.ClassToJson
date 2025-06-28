c7641a3e7302b47ea3c48dd22ed1c79834b0fedf
hadi mazareei <hadi.mazareei@live.com>
2025-06-28 14:46:33 +0330

    Add project files.

---
+++ Ces.ClassToJson/ClassToJson.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Ces.ClassToJson
+{
+    public class ClassToJson
+    {
+        private ClassToJsonOption _option;
+        private Assembly? _assembly;
+
+        public ClassToJson(ClassToJsonOption option)
+        {
+            _option = option;
+        }
+
+        public async Task CovertAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return;
+
+            if(string.IsNullOrEmpty(_option.AssemblyPath))
+                throw new ArgumentNullException(nameof(_option.AssemblyPath));
+
+            if(!_option.UseAssemblyPath && string.IsNullOrEmpty(_option.OutpuPath))
+                throw new ArgumentNullException(_option.OutpuPath);
+
+            _assembly = Assembly.LoadFrom(_option.AssemblyPath);
+        }
+
+        private async Task ConvertToJsonAsync()
+        {
+            var classes = _assembly.GetTypes();
+        }
+    }
+}
---
+++ Ces.ClassToJson/ClassToJsonOption.cs
@@ -0,0 +1,19 @@
+﻿namespace Ces.ClassToJson
+{
+    public class ClassToJsonOption
+    {
+        public string AssemblyPath { get; set; } = default!;
+        public string OutpuPath { get; set; } = default!;
+        /// <summary>
+        /// if true: Use [AssemblyPath] as [OutpuPath] to save result.
+        /// In this case [OutpuPath] can be null or empty
+        /// </summary>
+        public bool UseAssemblyPath { get; set; } = true;
+        public OutputTypeEnum OutputType { get; set; } = OutputTypeEnum.Json;
+    }
+
+    public enum OutputTypeEnum
+    {
+        Json
+    }
+}
