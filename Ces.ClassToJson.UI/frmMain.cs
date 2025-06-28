8133b6cc1cd474cafb3c577ae04278a57784c4ad
hadi mazareei <hadi.mazareei@live.com>
2025-06-28 15:06:57 +0330

    +add list box to UI

--- Ces.ClassToJson.UI/frmMain.cs
+++ Ces.ClassToJson.UI/frmMain.cs
@@ -1,10 +1,23 @@
-namespace Ces.ClassToJson.UI
+﻿namespace Ces.ClassToJson.UI
 {
     public partial class frmMain : Form
     {
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private void btnReadObjects_Click(object sender, EventArgs e)
+        {
+            var option = new Ces.ClassToJson.ClassToJsonOption
+            {
+                AssemblyPath = @"C:\Users\Caspian\Desktop\Ces.Caspian\Ces.Caspian.Models\bin\Debug\net8.0\Ces.Caspian.Models.dll"
+            };
+
+            var cls = new Ces.ClassToJson.ClassToJson(option);
+            var types = cls.CovertAsync(_cancellationTokenSource.Token);
         }
     }
 }
--- Ces.ClassToJson/ClassToJson.cs
+++ Ces.ClassToJson/ClassToJson.cs
@@ -15,19 +15,25 @@ namespace Ces.ClassToJson
         public async Task CovertAsync(CancellationToken cancellationToken)
         {
             if (cancellationToken.IsCancellationRequested)
-                return;
+                throw new OperationCanceledException();
 
-            if(string.IsNullOrEmpty(_option.AssemblyPath))
+            if (string.IsNullOrEmpty(_option.AssemblyPath))
                 throw new ArgumentNullException(nameof(_option.AssemblyPath));
 
             if(!_option.UseAssemblyPath && string.IsNullOrEmpty(_option.OutpuPath))
                 throw new ArgumentNullException(_option.OutpuPath);
 
             _assembly = Assembly.LoadFrom(_option.AssemblyPath);
+
+            if (_option.OutputType == OutputTypeEnum.Json)
+                await ConvertToJsonAsync(cancellationToken);
         }
 
-        private async Task ConvertToJsonAsync()
+        private async Task ConvertToJsonAsync(CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+                throw new OperationCanceledException();
+
             var classes = _assembly.GetTypes();
         }
     }
